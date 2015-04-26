991c18e266c92e85dc8aacd47c558c0ee0bc5cda
Ventura Daniela <daniela1988@gmail.com>
2015-04-26 16:39:19 +0200

    Fixed Parser to generate plan and getPlantInfo RESTdesc description

--- descriptions/use_case01/exec.sh
+++ descriptions/use_case01/exec.sh
@@ -1,11 +1,2 @@
-eye --traditional --quick-answere --tactic transaction --query goal02.n3 knowledge.n3 RESTdesc_descriptions/*/* > outputs/out.n3
-eye --traditional outputs/out.n3 parser/services.n3 --query parser/composition.n3 --nope
-
-
-
-
-
-
-#eye --traditional --quick-answere --tactic transaction --query goal02.n3 knowledge.n3 RESTdesc_descriptions/*/* > outputs/out.n3
-
-#eye --traditional outputs/out.n3 parser/services.n3 --query parser/composition.n3 --nope > outputs/parser.n3
+eye --traditional --quick-answere --tactic transaction --query goal03.n3 knowledge.n3 RESTdesc_descriptions/*/* > outputs/out.n3
+eye --traditional outputs/out.n3 parser/services.n3 --query parser/composition.n3 --nope > outputs/parser.n3
