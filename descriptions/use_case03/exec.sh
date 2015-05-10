181da88ed4cf4daf77e80ecb6ba9919c201a8a61
Ventura Daniela <daniela1988@gmail.com>
2015-05-10 17:18:15 +0200

    Resolved bug on HTTP requests to wait until they are finished and added goals for use_case 03 and 04

---
+++ descriptions/use_case03/exec.sh
@@ -0,0 +1,2 @@
+eye --traditional --quick-answere --tactic transaction --query goal04.n3 preference_numHours.n3 boards/* vocabularies/* inference/* services/* > outputs/out.n3
+eye --traditional outputs/out.n3 parser/services.n3 --query parser/composition.n3 --nope
